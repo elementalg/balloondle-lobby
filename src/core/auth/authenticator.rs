b81a2c640226a4c1f83b4d242e4d88a3d0dcff93
Gabriel Amihalachioaie <gamihalachioaie@gmail.com>
2021-06-17 12:22:53 +0200

    refactor: create user won't return anything

--- src/core/auth/authenticator.rs
+++ src/core/auth/authenticator.rs
@@ -4,5 +4,5 @@ use crate::core::auth::user::User;
 pub trait Authenticator {
     fn are_details_valid(&self, name: &String, code: &u32) -> Result<bool, AuthenticationError>;
     fn get_user_for_details(&self, name: &String, code: &u32) -> Result<User, AuthenticationError>;
-    fn create_user_with_details(&self, name: &String, code: &u32) -> User;
+    fn create_user_with_details(&self, name: &String, code: &u32);
 }
